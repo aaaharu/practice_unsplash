7055b7c77b4b7167218cccb49e81e4e31da498e8
Kim Eunji <qoopej@gmail.com>
2023-05-21 21:19:02 +0900

    commit

---
+++ practice_unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_unsplash
+//
+//  Created by 김은지 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
